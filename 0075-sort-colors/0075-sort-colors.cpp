1d9aaafb8c0c9342857d144637282817bef1f82b
Ayush Chahal <120248862+ayushchahal44@users.noreply.github.com>
2025-01-15 14:41:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int red=0,white=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                red++;
+            }
+            else if(nums[i]==1){
+                white++;
+            }
+        }
+        for(int i=0;i<red;i++){
+            nums[i]=0;
+        }
+        for(int i=red;i<nums.size()-white;i++){
+            nums[i]=1;
+        }
+        for(int i=red+white;i<nums.size();i++){
+            nums[i]=2;
+        }
     }
 };
